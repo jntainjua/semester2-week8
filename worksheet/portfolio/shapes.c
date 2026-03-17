9a484bd67429018ac2d4e26191b558623043d3ae
jntainjua <plnc0048@leeds.ac.uk>
2026-03-17 01:16:36 +0000

    updates

--- worksheet/portfolio/shapes.c
+++ worksheet/portfolio/shapes.c
@@ -1,4 +1,3 @@
-```c
 #include <stdbool.h>
 #include <math.h>
 
@@ -6,21 +5,21 @@
 
 /* Structure initialisation functions */
 
-Point makePoint( float x, float y ) {
+Point makePoint(float x, float y) {
     Point p;
     p.x = x;
     p.y = y;
     return p;
 }
 
-Line makeLine( Point p1, Point p2 ) {
+Line makeLine(Point p1, Point p2) {
     Line l;
     l.p[0] = p1;
     l.p[1] = p2;
     return l;
 }
 
-Triangle makeTriangle( Point p1, Point p2, Point p3 ) {
+Triangle makeTriangle(Point p1, Point p2, Point p3) {
     Triangle t;
     t.p[0] = p1;
     t.p[1] = p2;
@@ -30,13 +29,13 @@ Triangle makeTriangle( Point p1, Point p2, Point p3 ) {
 
 /* Structure calculation functions */
 
-float lineLength( Line l ) {
+float lineLength(Line l) {
     float dx = l.p[1].x - l.p[0].x;
     float dy = l.p[1].y - l.p[0].y;
     return sqrt(dx * dx + dy * dy);
 }
 
-float triangleArea( Triangle t ) {
+float triangleArea(Triangle t) {
     float x1 = t.p[0].x;
     float y1 = t.p[0].y;
     float x2 = t.p[1].x;
@@ -49,18 +48,17 @@ float triangleArea( Triangle t ) {
 
 /* Structure comparison functions */
 
-bool samePoint( Point p1, Point p2 ) {
+bool samePoint(Point p1, Point p2) {
     return fabs(p1.x - p2.x) < 1.0e-6 &&
            fabs(p1.y - p2.y) < 1.0e-6;
 }
 
-bool pointInLine( Point p, Line l ) {
+bool pointInLine(Point p, Line l) {
     return samePoint(p, l.p[0]) || samePoint(p, l.p[1]);
 }
 
-bool pointInTriangle( Point p, Triangle t ) {
+bool pointInTriangle(Point p, Triangle t) {
     return samePoint(p, t.p[0]) ||
            samePoint(p, t.p[1]) ||
            samePoint(p, t.p[2]);
 }
-```
